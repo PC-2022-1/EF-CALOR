95e41bac9dbc45db03347e6c6c3153a73cccbd29
ntamayoa <nataliatam9@gmail.com>
2022-06-02 22:51:52 -0500

    Cont. y Archivo prueba numerica

--- PruebaNumericaGarlekin.py
+++ PruebaNumericaGarlekin.py
@@ -1,5 +1,5 @@
 '''
-Se replica el metodo de garlekin para EF en una ecuacion del calor
+Prueba numerica con datos del libro para un ejemplo de 4 nodos (1 solo elemento)
 '''
 
 import numpy as np
@@ -7,10 +7,11 @@ import matplotlib.pyplot as plt
 from Mesh import * #Archivo donde esta la función para generar malla y para graficar
 from sympy import integrate, linear_eq_to_matrix, symbols,simplify,collect,  diff, Eq, Matrix
 from sympy import *
-#l = 0.6 #Distancia en x
-l=symbols('l')
-#w = 0.6 #Distancia en y
-w=symbols('w')
+
+l = 0.6 #Distancia en x
+#l=symbols('l')
+w = 0.6 #Distancia en y
+#w=symbols('w')
 p = 2  #Divisiones en x
 m = 2  #Divisiones en y
 tipoDeElemento = 'CUADRADO' #Puede ser elemento tipo 'TRIANGULO' o 'CUADRADO'
@@ -21,15 +22,21 @@ tipoDeElemento = 'CUADRADO' #Puede ser elemento tipo 'TRIANGULO' o 'CUADRADO'
 
 #Definicion de condiciones iniciales
 
-#q =1000 #Vatios/m3
-#k = 1.2 #conductividad en X y en Y (Watts)
-#h = 20  #Coeficiente de conveccion
-#Tf = 30 #Temperatura del aire 
-kx=symbols('kx')
-ky=symbols('kx')
-h=symbols('h')
-Tf=symbols('Tf')
-q=symbols('q')
+#q Vatios/m3
+#k conductividad en X y en Y (Watts)
+#h Coeficiente de conveccion
+#Tf Temperatura del aire 
+
+#kx=symbols('kx')
+kx=1.2
+#ky=symbols('kx')
+ky=1.2
+#h=symbols('h')
+h=20
+#Tf=symbols('Tf')
+Tf=30
+#q=symbols('q')
+q=1000
 
 #Dos caras aisladas y dos en contacto con el aire. Superficie es un foco de calor
 
@@ -46,11 +53,11 @@ def Si(x, y, l, w, i):
     if i==0: 
         return (1-(x/l))*(1-(y/w))
     elif i==1:
-        return  (x/l)*(1-y/w) 
+        return  (x/l)*(1-(y/w)) 
     elif i==2:
         return (x/l)*(y/w)
     elif i==3:
-        return (1-x/l)*(y/w)    
+        return (1-(x/l))*(y/w)    
 
 #Temperatura aproximada
 Ti=symbols('Ti')
@@ -61,9 +68,10 @@ Tn=symbols('Tn')
 def Taprox(x, y, l, w):
     return Ti + (x/l)*(Tj-Ti) + y/w *((Tn+(x/l)*(Tm-Tn)) - (Ti+(x/l)*(Tj-Ti)))
 
-''' integral(KxTerm + KyTerm + q)Sidxdy = 0  donde: 
-        KxTerm = Kxterm1 + Kxterm2
-        KyTerm = KyTerm1 + Kyterm2 '''
+''' CONSTRUCCION DE LA INTEGRAL: 
+    integral(KxTerm + KyTerm + q)Sidxdy = 0  donde: 
+    KxTerm = Kxterm1 + Kxterm2
+    KyTerm = KyTerm1 + Kyterm2 '''
 
 #print("Kxterm1")
 Kxterm1 = [0, 0, 0, 0]
@@ -97,7 +105,6 @@ for i in range (0, 4):
 #print("Kyterm2")
 Kyterm2=[0,0,0,0]
 for i in range (0, 4):
-    
     A=integrate(integrate(-ky*Ti*diff(Si(x,y,l,w,0),y)*diff(Si(x,y,l,w,i),y),(x,0,l)),(y,0,w))
     B=integrate(integrate(-ky*Tj*diff(Si(x,y,l,w,1),y)*diff(Si(x,y,l,w,i),y),(x,0,l)),(y,0,w))
     C=integrate(integrate(-ky*Tm*diff(Si(x,y,l,w,2),y)*diff(Si(x,y,l,w,i),y),(x,0,l)),(y,0,w))
@@ -107,11 +114,9 @@ for i in range (0, 4):
     
     Kyterm2[i] = sum
     Kyterm2[i] = simplify(sum)
-    #Kyterm2[i] = collect(sum, Ti)
-    #Kyterm2[i] = simplify(sum)
-    #print(Kyterm2[i])
 
-print("q")
+
+#print("q")
 qterm = [0,0,0,0]
 for i in range (0, 4):
     A = integrate(integrate(q*Si(x,y,l,w,i),(x,0,l)),(y,0,w))
@@ -122,14 +127,14 @@ for i in range (0, 4):
 eqSist=[0,0,0,0]
 for i in range (0,4):
     eqSist[i]= Kxterm1[i] + Kxterm2[i] + Kyterm1[i] + Kyterm2[i] + qterm[i] 
-    #print(eqSist[i])
+    print(eqSist[i])
     #print("--------")
 
 #Obteniendo el sistema de ecuaciones en forma matricial : (coeffMatrix)(Ti, Tn, Tj, Tm)trans + independentVector = 0
-coeffMatrix, independentVector = linear_eq_to_matrix(eqSist, [Ti, Tn, Tj, Tm])
+coeffMatrix, independentVector = linear_eq_to_matrix(eqSist, [Ti, Tj, Tm, Tn])
 print(coeffMatrix)
 print("------")
 print(independentVector)
 
 #Resolviendo el sistema de ecuaciones
-print(linsolve((coeffMatrix, independentVector), [Ti, Tn, Tj, Tm]))
+#print(linsolve((coeffMatrix, independentVector), [Ti, Tj, Tm, Tn]))
