7a6835616073656bb8d9ca96212cfc6f3518fa3a
Selene <ssolanov@unal.edu.co>
2022-06-07 15:49:38 -0500

    Condiciones de convección para cada elemento

--- GalerkinFuntion.py
+++ GalerkinFuntion.py
@@ -8,9 +8,10 @@ import matplotlib.pyplot as plt
 from Mesh import * #Archivo donde esta la función para generar malla y para graficar
 from sympy import integrate, linear_eq_to_matrix, symbols,simplify,collect,  diff, Eq, Matrix
 from sympy import *
+import pandas as pd
 
 
-def galerkinMethod(elemLength, elemWidth, NL, EL, h, Tf, kx, ky, q, i):
+def galerkinMethod(elemLength, elemWidth, NL, EL, h, Tf, kx, ky, q, i,totallenght,totalwidth):
 
     #Funciones de Galerkin para un elemento de 4 nodos
     x=symbols('x')
@@ -52,47 +53,78 @@ def galerkinMethod(elemLength, elemWidth, NL, EL, h, Tf, kx, ky, q, i):
 
     #For index, row
 
+    # NL[EL[0]-1][1],NL[EL[3]-1][1])
+
     EL=EL[i]
     T=[]
+    nodo=[]
     for j in EL:
         T.append(symbols("T"+str(j)))
+        nodo.append(j)
     print(T)
     print("-----------------------")
 
-    Kxterm1[0] = -h* integrate( Si(NL[EL[0]-1][0], y, elemLength, elemWidth, 0) * (Taprox(NL[EL[0]-1][0],y,elemLength,elemWidth,T) - Tf) ,( y, NL[EL[0]-1][1],NL[EL[3]-1][1]) ) 
+    # Kxterm1[0] = -h* integrate( Si(NL[EL[0]-1][0], y, elemLength, elemWidth, 0) * (Taprox(NL[EL[0]-1][0],y,elemLength,elemWidth,T) - Tf) ,( y, NL[EL[0]-1][1],NL[EL[3]-1][1]) ) 
+    # Kxterm1[0]= simplify(Kxterm1[0])
+    # Kxterm1[1] = -h* integrate( Si(NL[EL[1]-1][0], y, elemLength, elemWidth, 1) * (Taprox(NL[EL[1]-1][0],y,elemLength,elemWidth,T) - Tf) ,( y, NL[EL[0]-1][1],NL[EL[3]-1][1]) )
+    # Kxterm1[1]= simplify(Kxterm1[1])
+    # Kxterm1[2] = -h* integrate( Si(NL[EL[2]-1][0], y, elemLength, elemWidth, 2) * (Taprox(NL[EL[2]-1][0],y,elemLength,elemWidth,T) - Tf) ,( y, NL[EL[0]-1][1],NL[EL[3]-1][1]) )
+    # Kxterm1[2]= simplify(Kxterm1[2])
+    # Kxterm1[3] = -h* integrate( Si(NL[EL[3]-1][0], y, elemLength, elemWidth, 3) * (Taprox(NL[EL[3]-1][0],y,elemLength,elemWidth,T) - Tf) ,( y, NL[EL[0]-1][1],NL[EL[3]-1][1]) )
+    # Kxterm1[3]= simplify(Kxterm1[3])
+
+    Kxterm1[0] = -h* integrate( Si(0, y, elemLength, elemWidth, 0) * (Taprox(0,y,elemLength,elemWidth,T) - Tf) ,( y, 0 ,elemWidth) ) 
     Kxterm1[0]= simplify(Kxterm1[0])
-    Kxterm1[1] = -h* integrate( Si(NL[EL[1]-1][0], y, elemLength, elemWidth, 1) * (Taprox(NL[EL[1]-1][0],y,elemLength,elemWidth,T) - Tf) ,( y, NL[EL[0]-1][1],NL[EL[3]-1][1]) )
+    Kxterm1[1] = -h* integrate( Si(elemLength, y, elemLength, elemWidth, 1) * (Taprox(elemLength,y,elemLength,elemWidth,T) - Tf) ,( y, 0,elemWidth ) )
     Kxterm1[1]= simplify(Kxterm1[1])
-    Kxterm1[2] = -h* integrate( Si(NL[EL[2]-1][0], y, elemLength, elemWidth, 2) * (Taprox(NL[EL[2]-1][0],y,elemLength,elemWidth,T) - Tf) ,( y, NL[EL[0]-1][1],NL[EL[3]-1][1]) )
+    Kxterm1[2] = -h* integrate( Si(elemLength, y, elemLength, elemWidth, 2) * (Taprox(elemLength,y,elemLength,elemWidth,T) - Tf) ,( y, 0,elemWidth) )
     Kxterm1[2]= simplify(Kxterm1[2])
-    Kxterm1[3] = -h* integrate( Si(NL[EL[3]-1][0], y, elemLength, elemWidth, 3) * (Taprox(NL[EL[3]-1][0],y,elemLength,elemWidth,T) - Tf) ,( y, NL[EL[0]-1][1],NL[EL[3]-1][1]) )
+    Kxterm1[3] = -h* integrate( Si(0, y, elemLength, elemWidth, 3) * (Taprox(0,y,elemLength,elemWidth,T) - Tf) ,( y, 0,elemWidth) )
     Kxterm1[3]= simplify(Kxterm1[3])
+
     #Prueba conveccion: Para el primer elemento y cumplir con los lados de 
     #conveccion, se deben hacer estos dos terminos 0
-    #Kxterm1[2]=0
-    #Kxterm1[1]=0
+    # Kxterm1[2]=0
+    # Kxterm1[1]=0
     #print("Kyterm1")
+    # Kyterm1 = [0, 0, 0, 0]
+    # Kyterm1[0] = -h* integrate( Si(x, NL[EL[0]-1][1], elemLength, elemWidth, 0) * (Taprox(x,NL[EL[0]-1][1],elemLength,elemWidth,T) - Tf) ,( x, NL[EL[0]-1][0],NL[EL[1]-1][0]) )
+    # Kyterm1[0]= simplify(Kyterm1[0])
+    # Kyterm1[1] = -h* integrate( Si(x, NL[EL[1]-1][1], elemLength, elemWidth, 1) * (Taprox(x,NL[EL[1]-1][1],elemLength,elemWidth,T) - Tf) ,( x, NL[EL[0]-1][0],NL[EL[1]-1][0]) )
+    # Kyterm1[1]= simplify(Kyterm1[1])
+    # Kyterm1[2] = -h* integrate( Si(x, NL[EL[2]-1][1], elemLength, elemWidth, 2) * (Taprox(x,NL[EL[2]-1][1],elemLength,elemWidth,T) - Tf) ,( x, NL[EL[0]-1][0],NL[EL[1]-1][0]) )
+    # Kyterm1[2]= simplify(Kyterm1[2])
+    # Kyterm1[3] = -h* integrate( Si(x, NL[EL[3]-1][1], elemLength, elemWidth, 3) * (Taprox(x,NL[EL[3]-1][1],elemLength,elemWidth,T) - Tf) , ( x, NL[EL[0]-1][0],NL[EL[1]-1][0]))
+    # Kyterm1[3]= simplify(Kyterm1[3])
+
     Kyterm1 = [0, 0, 0, 0]
-    Kyterm1[0] = -h* integrate( Si(x, NL[EL[0]-1][1], elemLength, elemWidth, 0) * (Taprox(x,NL[EL[0]-1][1],elemLength,elemWidth,T) - Tf) ,( x, NL[EL[0]-1][0],NL[EL[1]-1][0]) )
+    Kyterm1[0] = -h* integrate( Si(x, 0, elemLength, elemWidth, 0) * (Taprox(x,0,elemLength,elemWidth,T) - Tf) ,( x, 0,elemLength) )
     Kyterm1[0]= simplify(Kyterm1[0])
-    Kyterm1[1] = -h* integrate( Si(x, NL[EL[1]-1][1], elemLength, elemWidth, 1) * (Taprox(x,NL[EL[1]-1][1],elemLength,elemWidth,T) - Tf) ,( x, NL[EL[0]-1][0],NL[EL[1]-1][0]) )
+    Kyterm1[1] = -h* integrate( Si(x, 0, elemLength, elemWidth, 1) * (Taprox(x,0,elemLength,elemWidth,T) - Tf) ,( x, 0,elemLength) )
     Kyterm1[1]= simplify(Kyterm1[1])
-    Kyterm1[2] = -h* integrate( Si(x, NL[EL[2]-1][1], elemLength, elemWidth, 2) * (Taprox(x,NL[EL[2]-1][1],elemLength,elemWidth,T) - Tf) ,( x, NL[EL[0]-1][0],NL[EL[1]-1][0]) )
+    Kyterm1[2] = -h* integrate( Si(x, elemWidth, elemLength, elemWidth, 2) * (Taprox(x,elemWidth,elemLength,elemWidth,T) - Tf) ,( x, 0,elemLength) )
     Kyterm1[2]= simplify(Kyterm1[2])
-    Kyterm1[3] = -h* integrate( Si(x, NL[EL[3]-1][1], elemLength, elemWidth, 3) * (Taprox(x,NL[EL[3]-1][1],elemLength,elemWidth,T) - Tf) , ( x, NL[EL[0]-1][0],NL[EL[1]-1][0]))
+    Kyterm1[3] = -h* integrate( Si(x, elemWidth, elemLength, elemWidth, 3) * (Taprox(x,elemWidth,elemLength,elemWidth,T) - Tf) , ( x, 0,elemLength))
     Kyterm1[3]= simplify(Kyterm1[3])
     #Prueba conveccion: Para el primer elemento y cumplir con los lados de 
     #conveccion, se deben hacer estos dos terminos 0
-    #Kyterm1[2]=0
-    #Kyterm1[3]=0
+    # Kyterm1[2]=0
+    # Kyterm1[3]=0
     #print("Kxterm2")
     Kxterm2=[0,0,0,0]
     for i in range (0, 4):
         
-        A=integrate(integrate(-kx*T[0]*diff(Si(x,y,elemLength,elemWidth,0),x)*diff(Si(x,y,elemLength,elemWidth,i),x),(x,NL[EL[0]-1][0],NL[EL[1]-1][0])),(y,NL[EL[0]-1][1],NL[EL[3]-1][1]))
-        B=integrate(integrate(-kx*T[1]*diff(Si(x,y,elemLength,elemWidth,1),x)*diff(Si(x,y,elemLength,elemWidth,i),x),(x,NL[EL[0]-1][0],NL[EL[1]-1][0])),(y,NL[EL[0]-1][1],NL[EL[3]-1][1]))
-        C=integrate(integrate(-kx*T[2]*diff(Si(x,y,elemLength,elemWidth,2),x)*diff(Si(x,y,elemLength,elemWidth,i),x),(x,NL[EL[0]-1][0],NL[EL[1]-1][0])),(y,NL[EL[0]-1][1],NL[EL[3]-1][1]))
-        D=integrate(integrate(-kx*T[3]*diff(Si(x,y,elemLength,elemWidth,3),x)*diff(Si(x,y,elemLength,elemWidth,i),x),(x,NL[EL[0]-1][0],NL[EL[1]-1][0])),(y,NL[EL[0]-1][1],NL[EL[3]-1][1]))
+        # A=integrate(integrate(-kx*T[0]*diff(Si(x,y,elemLength,elemWidth,0),x)*diff(Si(x,y,elemLength,elemWidth,i),x),(x,NL[EL[0]-1][0],NL[EL[1]-1][0])),(y,NL[EL[0]-1][1],NL[EL[3]-1][1]))
+        # B=integrate(integrate(-kx*T[1]*diff(Si(x,y,elemLength,elemWidth,1),x)*diff(Si(x,y,elemLength,elemWidth,i),x),(x,NL[EL[0]-1][0],NL[EL[1]-1][0])),(y,NL[EL[0]-1][1],NL[EL[3]-1][1]))
+        # C=integrate(integrate(-kx*T[2]*diff(Si(x,y,elemLength,elemWidth,2),x)*diff(Si(x,y,elemLength,elemWidth,i),x),(x,NL[EL[0]-1][0],NL[EL[1]-1][0])),(y,NL[EL[0]-1][1],NL[EL[3]-1][1]))
+        # D=integrate(integrate(-kx*T[3]*diff(Si(x,y,elemLength,elemWidth,3),x)*diff(Si(x,y,elemLength,elemWidth,i),x),(x,NL[EL[0]-1][0],NL[EL[1]-1][0])),(y,NL[EL[0]-1][1],NL[EL[3]-1][1]))
+
+        A=integrate(integrate(-kx*T[0]*diff(Si(x,y,elemLength,elemWidth,0),x)*diff(Si(x,y,elemLength,elemWidth,i),x),(x,0,elemLength)),(y,0,elemWidth))
+        B=integrate(integrate(-kx*T[1]*diff(Si(x,y,elemLength,elemWidth,1),x)*diff(Si(x,y,elemLength,elemWidth,i),x),(x,0,elemLength)),(y,0,elemWidth))
+        C=integrate(integrate(-kx*T[2]*diff(Si(x,y,elemLength,elemWidth,2),x)*diff(Si(x,y,elemLength,elemWidth,i),x),(x,0,elemLength)),(y,0,elemWidth))
+        D=integrate(integrate(-kx*T[3]*diff(Si(x,y,elemLength,elemWidth,3),x)*diff(Si(x,y,elemLength,elemWidth,i),x),(x,0,elemLength)),(y,0,elemWidth))
+
+
 
         sum = A+B+C+D
 
@@ -103,10 +135,18 @@ def galerkinMethod(elemLength, elemWidth, NL, EL, h, Tf, kx, ky, q, i):
     #print("Kyterm2")
     Kyterm2=[0,0,0,0]
     for i in range (0, 4):
-        A=integrate(integrate(-ky*T[0]*diff(Si(x,y,elemLength,elemWidth,0),y)*diff(Si(x,y,elemLength,elemWidth,i),y),(x,NL[EL[0]-1][0],NL[EL[1]-1][0])),(y,NL[EL[0]-1][1],NL[EL[3]-1][1]))
-        B=integrate(integrate(-ky*T[1]*diff(Si(x,y,elemLength,elemWidth,1),y)*diff(Si(x,y,elemLength,elemWidth,i),y),(x,NL[EL[0]-1][0],NL[EL[1]-1][0])),(y,NL[EL[0]-1][1],NL[EL[3]-1][1]))    
-        C=integrate(integrate(-ky*T[2]*diff(Si(x,y,elemLength,elemWidth,2),y)*diff(Si(x,y,elemLength,elemWidth,i),y),(x,NL[EL[0]-1][0],NL[EL[1]-1][0])),(y,NL[EL[0]-1][1],NL[EL[3]-1][1]))
-        D=integrate(integrate(-ky*T[3]*diff(Si(x,y,elemLength,elemWidth,3),y)*diff(Si(x,y,elemLength,elemWidth,i),y),(x,NL[EL[0]-1][0],NL[EL[1]-1][0])),(y,NL[EL[0]-1][1],NL[EL[3]-1][1]))
+        # A=integrate(integrate(-ky*T[0]*diff(Si(x,y,elemLength,elemWidth,0),y)*diff(Si(x,y,elemLength,elemWidth,i),y),(x,NL[EL[0]-1][0],NL[EL[1]-1][0])),(y,NL[EL[0]-1][1],NL[EL[3]-1][1]))
+        # B=integrate(integrate(-ky*T[1]*diff(Si(x,y,elemLength,elemWidth,1),y)*diff(Si(x,y,elemLength,elemWidth,i),y),(x,NL[EL[0]-1][0],NL[EL[1]-1][0])),(y,NL[EL[0]-1][1],NL[EL[3]-1][1]))    
+        # C=integrate(integrate(-ky*T[2]*diff(Si(x,y,elemLength,elemWidth,2),y)*diff(Si(x,y,elemLength,elemWidth,i),y),(x,NL[EL[0]-1][0],NL[EL[1]-1][0])),(y,NL[EL[0]-1][1],NL[EL[3]-1][1]))
+        # D=integrate(integrate(-ky*T[3]*diff(Si(x,y,elemLength,elemWidth,3),y)*diff(Si(x,y,elemLength,elemWidth,i),y),(x,NL[EL[0]-1][0],NL[EL[1]-1][0])),(y,NL[EL[0]-1][1],NL[EL[3]-1][1]))
+
+
+        
+        A=integrate(integrate(-ky*T[0]*diff(Si(x,y,elemLength,elemWidth,0),y)*diff(Si(x,y,elemLength,elemWidth,i),y),(x,0,elemLength)),(y,0,elemWidth))
+        B=integrate(integrate(-ky*T[1]*diff(Si(x,y,elemLength,elemWidth,1),y)*diff(Si(x,y,elemLength,elemWidth,i),y),(x,0,elemLength)),(y,0,elemWidth))    
+        C=integrate(integrate(-ky*T[2]*diff(Si(x,y,elemLength,elemWidth,2),y)*diff(Si(x,y,elemLength,elemWidth,i),y),(x,0,elemLength)),(y,0,elemWidth))
+        D=integrate(integrate(-ky*T[3]*diff(Si(x,y,elemLength,elemWidth,3),y)*diff(Si(x,y,elemLength,elemWidth,i),y),(x,0,elemLength)),(y,0,elemWidth))
+
 
         sum = A+B+C+D
         
@@ -118,21 +158,134 @@ def galerkinMethod(elemLength, elemWidth, NL, EL, h, Tf, kx, ky, q, i):
     #print("q")
     qterm = [0,0,0,0]
     for i in range (0, 4):
-        A = integrate(integrate(q*Si(x,y,elemLength,elemWidth,i),(x,NL[EL[0]-1][0],NL[EL[1]-1][0])),(y,NL[EL[0]-1][1],NL[EL[3]-1][1]))
+        #A = integrate(integrate(q*Si(x,y,elemLength,elemWidth,i),(x,NL[EL[0]-1][0],NL[EL[1]-1][0])),(y,NL[EL[0]-1][1],NL[EL[3]-1][1]))
+        A = integrate(integrate(q*Si(x,y,elemLength,elemWidth,i),(x,0,elemLength)),(y,0,elemWidth))
         qterm[i] = simplify(A)
         #print(qterm[i])
 
     #Reuniendo el Sistema de ecuaciones para cada nodo juntando los terminos 
     eqSist=[0,0,0,0]
     listaEjemplo=[0,0,0,0,0,0,0,0,0]
+    eqSist2=[0,0,0,0]
     for i in range (0,4):
-        eqSist[i]= Kxterm1[i] + Kxterm2[i] + Kyterm1[i] + Kyterm2[i] + qterm[i] 
-        print(eqSist[i])
-        #print("--------")
+        # eqSist[i]= Kxterm1[i] + Kxterm2[i] + Kyterm1[i] + Kyterm2[i] + qterm[i] 
+        eqSist[i]= Kxterm2[i] + Kyterm2[i] + qterm[i] 
+        #eqSist2[i]= Kxterm1[i] + Kyterm1[i] + eqSist[i]
+        # print(eqSist[i])
+        # print("--------")
+        
     #Obteniendo el sistema de ecuaciones en forma matricial : (coeffMatrix)(Ti, Tn, Tj, Tm)trans + independentVector = 0
-    coeffMatrix, independentVector = linear_eq_to_matrix(eqSist, [T[0], T[1], T[2], T[3]])
+    # coeffMatrix, independentVector = linear_eq_to_matrix(eqSist, [T[0], T[1], T[2], T[3]])
+
+        #Obteniendo el sistema de ecuaciones en forma matricial : (coeffMatrix)(Ti, Tn, Tj, Tm)trans + independentVector = 0
+
+    Mk, Mq = linear_eq_to_matrix(eqSist, [T[0], T[1], T[2], T[3]])
+    Mx,Vx = linear_eq_to_matrix(Kxterm1, [T[0], T[1], T[2], T[3]])
+    My, Vy = linear_eq_to_matrix(Kyterm1, [T[0], T[1], T[2], T[3]])
+
     #print(coeffMatrix)
-    #print("------")
+    # print("------")
+    # #print(independentVector)
+    # print(Mk)
+    # print(Mq)
+    # print(Mx)
+    # print(Vx)
+    # print(My)
+    # print(Vy)
+    #print(B)
+
+    # print('Mx original:')
+    # print(Mx)
+    # print('My original:')
+    # print(My)
+
+    #Condiciones de convección en Mx,Vx y My,Vy
+    for i in range(0,4):
+
+        # print('-------> NODO'+str(i))
+
+        if(NL[EL[i]-1][0]!=0 and NL[EL[i]-1][0]!=totallenght and NL[EL[i]-1][1]!=0 and NL[EL[i]-1][1]!=totalwidth):
+            # print('internos')
+            # print(NL[EL[i]-1])
+            # Mx=zeros(4,4)
+            # My=zeros(4,4)
+            # Vy=zeros(4,1)
+            # Vx=zeros(4,1)
+
+            Mx[i,0]=0
+            Mx[i,1]=0
+            Mx[i,2]=0
+            Mx[i,3]=0
+            Vx[i,0]=0
+            My[i,0]=0
+            My[i,1]=0
+            My[i,2]=0
+            My[i,3]=0
+            Vy[i,0]=0
+
+            # print('Mx interno:')
+            # print(Mx)
+            # print('My interno:')
+            # print(My)
+
+        
+        if(NL[EL[i]-1][0]==0 or NL[EL[i]-1][0]==totallenght):
+            if(NL[EL[i]-1][1]!=0 and NL[EL[i]-1][1]!=totalwidth):
+                Mx[i,0]=0
+                Mx[i,1]=0
+                Mx[i,2]=0
+                Mx[i,3]=0
+                Vx[i,0]=0
+                # print('Mx')
+                # print(NL[EL[i]-1])
+
+                # print('Mx en Mx=0:')
+                # print(Mx)
+                # print('My en Mx=0:')
+                # print(My)
+
+
+        if(NL[EL[i]-1][1]==0 or NL[EL[i]-1][1]==totalwidth):
+            if(NL[EL[i]-1][0]!=0 and NL[EL[i]-1][0]!=totallenght):
+                print('My')
+                My[i,0]=0
+                My[i,1]=0
+                My[i,2]=0
+                My[i,3]=0
+                Vy[i,0]=0
+                #print(NL[EL[i]-1])
+
+                #print('Mx en My=0:')
+                #print(Mx)
+                #print('My en My=0:')
+                #print(My)
+  
+
+    # My[1]=[0,0,0,0]
+    # Vy[1]=[0]
+    # Mx[3]=[0,0,0,0]
+    # Vx[3]=[0]
+
+   
+    coeffMatrix =Matrix(Mk+Mx+My)
+    independentVector=Mq + Vx + Vy
+    #print(coeffMatrix)
+ 
+    print(coeffMatrix)
+    print(independentVector)
+    print("------")
+
+    dfelement=pd.DataFrame(np.matrix(coeffMatrix))
+    dfelement["indep"]=pd.DataFrame(np.matrix(independentVector))
+    dfelement["nodo"]=pd.DataFrame(np.array(nodo))
+
+    dfelement.rename(columns = {0: T[0],  1 :T[1], 2 :T[2], 3 :T[3] }, inplace = True)
+    # print(Mk)
+    # print(Mq)
+    # print(Mx)
+    # print(Vx)
+    # print(My)
+    # print(Vy)
 
     #g=0
     #for j in EL:
@@ -143,6 +296,7 @@ def galerkinMethod(elemLength, elemWidth, NL, EL, h, Tf, kx, ky, q, i):
 
     #Resolviendo el sistema de ecuaciones
     #print(linsolve((coeffMatrix, independentVector), [T[0], T[1], T[2], T[3]]))
-    return eqSist
+    return dfelement
+
 
 
--- GalerkinTest.py
+++ GalerkinTest.py
@@ -2,10 +2,12 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 from Mesh import * #Archivo donde esta la función para generar malla y para graficar
 from sympy import integrate, linear_eq_to_matrix, symbols,simplify,collect,  diff, Eq, Matrix
 from sympy import *
 from GalerkinFuntion import *
+from GalerkinFuntionNormalizado import *
 
 l = 0.6 #Distancia en x
 #l=symbols('l')
@@ -18,9 +20,11 @@ elemLength=l/p
 elemWidth=w/m
 tipoDeElemento = 'CUADRADO' #Puede ser elemento tipo 'TRIANGULO' o 'CUADRADO'
 
-NL,EL = uniform_mesh(l,w,p,m,tipoDeElemento) #Generar malla
+# NL,EL = uniform_mesh(l,w,p,m,tipoDeElemento) #Generar malla
 
-graph_mesh(tipoDeElemento,NL,EL) #Graficar malla
+NL= [[0,0],[0.3,0],[0.3,0.3],[0,0.3],[-0.3,0.3],[-0.3,0],[-0.3,-0.3],[0,-0.3],[0.3,-0.3]]
+EL=[[1,2,3,4],[6,1,4,5],[7,8,1,6],[8,9,2,1]]
+# graph_mesh(tipoDeElemento,NL,EL) #Graficar malla
 
 #Definicion de condiciones iniciales
 
@@ -40,14 +44,41 @@ Tf=30
 #q=symbols('q')
 q=1000
 
-print(EL)
-print(NL)
+# print(EL)
+# print(NL)
 eqSist=[]
-for i in range (0, len(EL)):
-    galerkinMethod(elemLength, elemWidth, NL, EL, h, Tf, kx, ky, q, i)
-    #eqSist.append(galerkinMethod(elemLength, elemWidth, NL, EL, h, Tf, kx, ky, q, i))
-    #print(eqSist)#
+dataFrame=pd.DataFrame()
+dataF1=pd.DataFrame()
+dataF2=pd.DataFrame()
+dataF3=pd.DataFrame()
+dataF4=pd.DataFrame()
 
-#eqSist = (galerkinMethod(elemLength, elemWidth, NL, EL, h, Tf, kx, ky, q, 0)) #Para prueba conveccion primer elemento
+# for i in range (0, len(EL)):
+#      (galerkinMethod(elemLength, elemWidth, NL, EL, h, Tf, kx, ky, q, i,l,w))
+#     #eqSist.append(galerkinMethod(elemLength, elemWidth, NL, EL, h, Tf, kx, ky, q, i))
+#     #print(eqSist)#
 
-#print(eqSist)
+# dataF1 = (galerkinMethodNorm(elemLength, elemWidth, NL, EL, h, Tf, kx, ky, q, 0,l,w))
+# dataF2 = (galerkinMethodNorm(elemLength, elemWidth, NL, EL, h, Tf, kx, ky, q, 1,l,w))
+# dataF3 = (galerkinMethodNorm(elemLength, elemWidth, NL, EL, h, Tf, kx, ky, q, 2,l,w))
+# dataF4 = (galerkinMethodNorm(elemLength, elemWidth, NL, EL, h, Tf, kx, ky, q, 3,l,w))
+
+dataF1 = (galerkinMethod(elemLength, elemWidth, NL, EL, h, Tf, kx, ky, q, 0,l,w))
+dataF2 = (galerkinMethod(elemLength, elemWidth, NL, EL, h, Tf, kx, ky, q, 1,l,w))
+dataF3 = (galerkinMethod(elemLength, elemWidth, NL, EL, h, Tf, kx, ky, q, 2,l,w))
+dataF4 = (galerkinMethod(elemLength, elemWidth, NL, EL, h, Tf, kx, ky, q, 3,l,w))
+
+result = pd.concat([dataF1, dataF2, dataF3, dataF4], axis=0)
+
+# result.groupby(['indep'])['indep'].sum()
+#result.fillna(0)
+algo = result.replace(np.nan,0)
+algo = result.groupby("nodo").sum()
+
+algo.to_excel("ef.xlsx")
+result.to_excel ("ef2.xlsx" )
+print(algo)
+
+# eqSist = (galerkinMethod(elemLength, elemWidth, NL, EL, h, Tf, kx, ky, q, 0)) #Para prueba conveccion primer elemento
+
+# print(eqSist)
