a1728f6d3651356771d23715a34b6e35d579a30e
ntamayoa <nataliatam9@gmail.com>
2022-06-18 11:00:49 -0500

    definicion funciones

--- MatlabTry.py
+++ MatlabTry.py
@@ -16,32 +16,77 @@ import matplotlib.animation as animation
 import seaborn as sns
 from scipy.sparse import csr_matrix
 
-def stima3(nodosElemento,newNL ):
-    vertices=[]
-    for element in nodosElemento:
-        vertices.append(newNL[element-1]) 
+# def stima3(nodosElemento,newNL ):
+#     vertices=[]
+#     for element in nodosElemento:
+#         vertices.append(newNL[element-1]) 
     
-    vertices=np.matrix(vertices)
-    print(vertices)
-    d=3
-    # vertices = np.insert(vertices, 0, col, axis=1)
-    # vertices = np.insert(vertices, 0, row, axis=0)
-    M1=np.vstack((np.ones((1,d+1)),vertices.transpose()))   
-    print(M1)
-    A=np.zeros((1,d))
-    B=np.eye(d)
-    M2=np.vstack((A,B))
-    #print(M2)
-    print(M2)
-    G=np.linalg.solve((M1),(M2))
-    print(G)
-    #print(G)
-    a=[1,2,3]
-    # MM=np.linalg.det(np.vstack((np.ones((1,d+1)),vertices.transpose())))
+#     vertices=np.matrix(vertices)
+#     print(vertices)
+#     d=3
+#     # vertices = np.insert(vertices, 0, col, axis=1)
+#     # vertices = np.insert(vertices, 0, row, axis=0)
+#     M1=np.vstack((np.ones((1,d+1)),vertices.transpose()))   
+#     print(M1)
+#     A=np.zeros((1,d))
+#     B=np.eye(d)
+#     M2=np.vstack((A,B))
+#     #print(M2)
+#     print(M2)
+#     G=np.linalg.solve((M1),(M2))
+#     print(G)
+#     #print(G)
+#     a=[1,2,3]
+#     # MM=np.linalg.det(np.vstack((np.ones((1,d+1)),vertices.transpose())))
+#     MM=np.linalg.det(np.vstack((np.ones((1,d+1)),vertices.transpose())))*G
+#     MM=np.dot(MM,G.transpose())
+#     M=MM/6    
+#     return M
+
+
+
+def stima3(vertices):
+    d  = np.size(vertices, 1)
+
+    G1 = np.vstack((np.ones((1,d+1)),vertices.transpose()))  
+    G2 = np.vstack((np.zeros((1,d)),np.eye(d)))
+    G = np.linalg.solve(G1, G2)
+
     MM=np.linalg.det(np.vstack((np.ones((1,d+1)),vertices.transpose())))*G
     MM=np.dot(MM,G.transpose())
-    M=MM/6    
+    M=MM / np.prod([i for i in range (0,d)])    
+
     return M
+
+def stima4(vertices):
+
+    D_phi = np.array([[vertices[1,:] - vertices[0,:]], [vertices[3,:] - vertices[0,:]]])
+    D_phiTrans =D_phi.transpose()
+   
+    B = np.linalg.inv (np.dot (D_phi, D_phiTrans) )
+
+    C1matrix= [[[2, -2],[-2,2]], [[3, 0],[0,-3]],  [[2, 1],[1,2]] ]
+    C1 = np.matrix(C1matrix[0]) * B[0,0] + np.matrix(C1matrix[1]) * B[0,1] + np.matrix(C1matrix[3]) * B[1,1] 
+
+    C2matrix= [[[-1, 1],[1,-1]], [[-3, 0],[0,3]],  [[-1, -2],[-2,-1]] ]
+    C2 = np.matrix(C2matrix[0]) * B[0,0] + np.matrix(C2matrix[1]) * B[0,1] + np.matrix(C2matrix[3]) * B[1,1] 
+
+    M = (np.linalg.det(D_phiTrans) * np.matrix ([C1, C2], [C2, C1])) / 6
+    return M
+
+
+def f(u): 
+    value = np.ones(np.size(u, 0), 0)
+    return value
+
+def g(u): 
+    value = np.zeros(np.size(u, 0), 0)
+    return value
+
+def u_d(u): 
+    value = np.zeros(np.size(u, 0), 0)
+    return value
+
 
 l = 0.6 #Distancia en x
 #l=symbols('l')
