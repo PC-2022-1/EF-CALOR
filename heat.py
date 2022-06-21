e510b5091f5c9990180edeb98d34de978bb6ca98
Selene <ssolanov@unal.edu.co>
2022-06-21 17:35:24 -0500

    Ajustes finales

--- heat.py
+++ heat.py
@@ -4,60 +4,62 @@ from Mesh import *
 from heatFunction import *
 from scipy.sparse import *
 
-## --- Testing phase --- ##
-
-# Dimentional Parameters
+# Parametros para el mallado
 l = 4 # Distancia en x
 w = 4 # Distancia en y
-p = 10 # Divisiones en x
-m = 10 # Divisiones en y
+p = 30 # Divisiones en x
+m = 30 # Divisiones en y
 
-# Type Parameters
+# Tipo de elemento
 tipoDeElemento = 'TRIANGULO'
 
-# Behaviour Parameters
-f_fun = lambda x : 0
-g_fun = lambda x : 10
-u_d_fun = lambda x : 100
+# Parametros de comportamientos
+f_fun = lambda x : 0 #Funcion laplacianoU=f
+g_fun = lambda x : 2 #Derivada de la temperatura en lados con condiciones neumann. g=0 es lado aislado
+u_d_fun = lambda x : 100 #Temperaturas fijas en lados dirichlet
 
-# --- Se generan los archivos a cargar --- ##
+# --- ----------------discretización --------------------- ##
 
+#NL es una lista con las coordenadas de cada elemento
+#EL es una lista con los nodos de cada elemento
 NL, EL = uniform_mesh(l, w, p, m, tipoDeElemento)
 
+#Se convierte la lista NL en un arreglo
 coordinates = array(NL)
 
+#Se escoge la lista con los nodos dependiendo del tipo de elemento
 if tipoDeElemento == 'CUADRADO':
     elements4 = EL
 else:
     elements3 = EL
 
-## --- Se generan Newman & Dirichlet --- ##
-# Lados i-j, j-m, m-n, n-i Que tipo de condicion de frontera
+
+#----------------------Neumann & Dirichlet-----------------------#
+# Se generan listas que contienen condiciones 
+# Tipo de condicion de frontera: Lado inferior, derecho, superior, izquierdo 
 # Si es True es Neumann, si es False es dirichlet
-
-condicionesDeFrontera = [False, True, True, True]  #i-j n-i neumann, #j-m, m-n dirichlet
-
+condicionesDeFrontera = [True, False, True, False] 
 neumann,dirichlet = neumannOrDirichlet(condicionesDeFrontera, EL, NL, w, l)
 
-# if tipoDeElemento == 'TRIANGULO':
-#     neumann   =np.array([[1,4],[1,2],[2,3],[4,7]])
-#     dirichlet =np.array([[3,6],[6,9],[9,8],[8,7]])
-# else: neumann,dirichlet = neumannOrDirichlet(condicionesDeFrontera, EL, NL, w, l)
 
-# Generación de Matriz A & b
+
+# Pasos iniciales para generación de Matriz A & b
 
 ZeroMatrix = np.zeros((size(coordinates, 0) , size(coordinates, 0)))
 A = lil_matrix(ZeroMatrix)
 aux = lil_matrix(ZeroMatrix)
 b = zeros((size(coordinates, 0), 1))
 
-## --- Assembly --- ##
+## ---------------- ENSAMBLE-------------------- --- ##
 
-#-----------A-------------#
-#for j in range(size(elements3, 0)):
-#    A[elements4[j, :], elements4[j, :]] = A[elements3[j, :], elements3[j, :]] + stima3(coordinates[elements3[j, :], :])
 
-if tipoDeElemento == 'CUADRADO':
+#------------Matriz de esfuerzos A-------------------##
+
+#dependiendo del tipo de elemento se llama a la función stima3/stima4 y se va actualizando la matriz A (sparse) con los
+#valores que devuelve cada función. Si hay alguna posición repetida en la matriz se suma el valor. Esto se hace para cada 
+#elemento.
+
+if tipoDeElemento == 'CUADRADO': 
     for j in range(size(elements4, 0)):
         k=0
         for i in elements4[j]-1:
@@ -75,55 +77,65 @@ else:
         aux =lil_matrix(ZeroMatrix)
 
 
-#-----------b-------------#
-# Volume Forces
-#for j in range(size(elements3, 0)):
-#    b[elements3[j, :]] = b[elements3[j, :]] + linalg.det(vstack(ones(1, 3), coordinates[elements3[j,:], :].T)) * f(sum(coordinates[elements3[j,:], :]) / 3) / 6
+#---------------------vector b-----------------------##
 
+# se construye el vector b involucrando la funcion f (laplacianoU = f). Se soluciona la integral de la función f multiplicado el vector de la función Hat
+# por medio de una aproximación que involucra el determinante de una matriz que corresponde al area del elemento y la evaluacion de f en el centro de masa
 if tipoDeElemento == 'CUADRADO':
     for j in range(size(elements4, 0)):
-        H = np.vstack((ones((1, 3)), coordinates[elements4[j,:3]-1, :].T))
-        
+        H = np.vstack((ones((1, 3)), coordinates[elements4[j,:3]-1, :].T)) 
         b[elements4[j, :]-1] = b[elements4[j, :]-1] + linalg.det(H) * f(sum(coordinates[elements4[j,:]-1, :], axis=0) / 4, f_fun)  / 4
-        #b[elements4[j, :]-1] = b[elements4[j, :]-1] + linalg.det(H) * 1 / 4
+
 else: 
     for j in range(size(elements3, 0)):
         H = np.vstack((ones((1, 3)), coordinates[elements3[j,:]-1, :].T))
         b[elements3[j, :]-1] = b[elements3[j, :]-1] + linalg.det(H) * f(sum(coordinates[elements3[j,:]-1, :], axis=0) / 3, f_fun)  / 6
 
 
-# Neumann conditions
+##---------------- condiciones Neumann------------------------------##
+
+#Se involucran las condiciones de frontera tipo neumann. modifica el vector b agregando el termino que contiene a la función g. Se hace tambien
+#Una aproximación del punto medio y un determinante con la longitud del elemento
+
 if len(neumann) > 0:
     for j in range(size(neumann, 0)):
         b[neumann[j][:]-1] = b[neumann[j][:]-1] + linalg.norm(coordinates[neumann[j][0]-1, :] - coordinates[neumann[j][1]-1, :]) * g(sum(coordinates[neumann[j,:]-1, :], axis=0) / 2, g_fun) / 2
-        #b[neumann[j][:]-1] = b[neumann[j][:]-1] + linalg.norm(coordinates[neumann[j][0]-1,:] - coordinates[neumann[j][1]-1, :] ) * 0 / 2
 
 
+
+
+#Se genera el vector de incognitas u
 u = zeros_like(b)
 u = u[:, 0].reshape(b.shape)
 
+##---------------- condiciones dirchlet----------------------------------##
+
+#Se reemplazan los valores en u de las incognitas cuyos valores son conocidos
 BoundNodes = array(unique(dirichlet)) -1
 u[BoundNodes] = u_d(coordinates[BoundNodes, :], u_d_fun)
+
+#Se reescribe el primer bloque de ecuaciones
 b = b - A * u
 
-FreeNodes = setdiff1d(range(size(coordinates, 0)), BoundNodes)
+##-----------------solucion del sistema----------------------------------##
 
+#Se soluciona el sistema de ecuaciones con la inversa de la matriz A 
+FreeNodes = setdiff1d(range(size(coordinates, 0)), BoundNodes)
 u[FreeNodes] = linalg.inv(A[FreeNodes][:, FreeNodes].toarray()) @ b[FreeNodes]
 u = u[:, 0].reshape(coordinates[:, -1].shape)
+print(u)
 
-
-# show(elements4, coordinates, u)
+##---------------------Grafica-----------------------------------------##
 if tipoDeElemento=='CUADRADO':
-    matrixCalor = show(elements4, coordinates, u, 'CUADRADO', p)
+    matrixCalor = show(coordinates, u, 'CUADRADO', p)
 else:
-    matrixCalor = show(elements3, coordinates, u, 'TRIANGULO', p)
+    matrixCalor = show(coordinates, u, 'TRIANGULO', p)
 
+
+#Para hacer pruebas con funciones teoricas
 matrixTeorica = np.zeros(array(matrixCalor).shape)
-
 for row, Row in enumerate(matrixTeorica):
     for column, Column in enumerate(Row):
         matrixTeorica[row, column] = l/p * row + w/m * column
-
 #print( matrixTeorica - matrixCalor )
 print( linalg.norm(matrixTeorica - matrixCalor) /  linalg.norm(matrixTeorica) )
-print(u)
--- heatFunction.py
+++ heatFunction.py
@@ -13,8 +13,8 @@ from matplotlib import cm
 #Funciones f, g y u_d
 #Y funciones para graficas en 2d y 3d (3er eje temperatura)
 
-def stima3(vertices):
-    d  = np.size(vertices, 1)
+def stima3(vertices): #Genera la matriz de esfuerzos para un elemento tipo triangular
+    d  = np.size(vertices, 1) 
 
     G1 = np.vstack((np.ones((1,d+1)),vertices.transpose()))  
     G2 = np.vstack((np.zeros((1,d)),np.eye(d)))
@@ -26,7 +26,7 @@ def stima3(vertices):
 
     return M
 
-def stima4(vertices):
+def stima4(vertices): #Genera la matriz de esfuerzos para un elemento tipo cuadrado
 
     D_phi = np.vstack((vertices[1,:] - vertices[0,:], vertices[3,:] - vertices[0,:]))
     D_phiTrans = D_phi.transpose()
@@ -43,19 +43,20 @@ def stima4(vertices):
     M = (np.linalg.det(D_phiTrans) * np.vstack((np.hstack((C1, C2)), np.hstack((C2, C1))))) / 6
     return M
 
+
 def f(u, fun): 
     w = u.copy()
-    # w = w[0]+w[-1] # Tipo de argumento
+    w = w[0]+w[-1]  #Argumento que entra a la funcion.
     value = fun(w)
     return value
 
-def g(u, fun):
+def g(u, fun): #Valor derivada u en fronteras neumann
     w = u.copy()
-    # w = w[0]+w[-1] # Tipo de argumento
+    w = w[0]+w[-1]  #Argumento que entra a la funcion.
     value = fun(w)
     return value
 
-def u_d(u, fun):
+def u_d(u, fun):  #Valor en las fronteras dirichlet
     w = u.copy()
     # v=w[:,-1]
     # ww=w[:,0]
@@ -65,12 +66,12 @@ def u_d(u, fun):
     value = fun(w)
     return value
 
-def show(elementsx, coordinates, u, tipodeElemento, p):
+def show(coordinates, u, tipodeElemento, p): #Para un elemento tipo triangular genera una grafica 3d, para un elemento tipo cuadrado genera una grafica 2d
     
     matrixCalor = []
-    v=u.copy()
+    v =u.copy()
     while v.size > 0:
-        matrixCalor.append(v[:p+1])
+        matrixCalor.append(v[:p+1])  #Matriz en orden para graficar en 2d con sns
         v = v[p+1:]
     if tipodeElemento == "CUADRADO":
         ax = sns.heatmap(matrixCalor, linewidth=0.01,cmap="Spectral_r",  cbar_kws={'label': 'Temperatura °C'})
@@ -79,7 +80,6 @@ def show(elementsx, coordinates, u, tipodeElemento, p):
     else:
         fig = plt.figure()
         ax = fig.add_subplot(projection= '3d')
-
         X, Y = coordinates[:, 0], coordinates[:, -1]
         result = ax.plot_trisurf(X, Y, u, linewidth=0.2,  cmap=plt.cm.Spectral_r)
         fig.colorbar(result)
@@ -88,14 +88,21 @@ def show(elementsx, coordinates, u, tipodeElemento, p):
 
 
 def neumannOrDirichlet(condicionesDeFrontera, EL, NL, w, l):
-    #Lados i-j, j-m, m-n, n-i Que tipo de condicion de frontera
-    #Si es True es Neumann, si es False es dirichlet
+    # Se generan listas que contienen condiciones 
+    # lista condicionesDeFrontera: Lado inferior, derecho, superior, izquierdo 
+    # Si es True es Neumann, si es False es dirichlet
     neumann=[]
     dirichlet=[]
     listaAuxX=[]
     listaAuxY=[]
     listaAuxX_D=[]
     listaAuxY_D=[]
+
+    #Recorre cada elemento y cada nodo del elemento, verifica si se encuentra en una frontera
+    #Y luego el tipo de condicion de la misma. 
+    #Deacuerdo a eso se guarda en una lista diferente que se retorna al final
+
+    #Se usan listas auxiliares para guardar dos nodos por elemento en la lista
 
     for element in EL:
         for nodo in element:
